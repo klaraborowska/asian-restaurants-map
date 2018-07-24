a48716ef73da85efdac9acec220c715f797e7223
klaraborowska <33237744+klaraborowska@users.noreply.github.com>
2018-07-24 09:23:11 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">Neighborhood Map</header>
+        <div className="wrapper">
+          <aside className="side-list">
+            <input type="text" value="Search" id="input" className="search" />
+            <ul className="list">
+              <li className="list-item">Wilanów</li>
+              <li className="list-item">Mokotów</li>
+              <li className="list-item">Centrum</li>
+              <li className="list-item">Praga</li>
+              <li className="list-item">Ursus</li>
+              <li className="list-item">Włochy</li>
+              <li className="list-item">Białołęka</li>
+              <li className="list-item">Wola</li>
+              <li className="list-item">Żoliborz</li>
+              <li className="list-item">Bielany</li>
+            </ul>
+          </aside>
+          <div className="map"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
