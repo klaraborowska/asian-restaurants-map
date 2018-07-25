d75ec818498fcade04abb905843c3d6821512d6b
klaraborowska <33237744+klaraborowska@users.noreply.github.com>
2018-07-25 16:49:08 +0200

    Add Info Window

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,15 +2,25 @@ import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
-
   state = {
     points: [
-      { lat: 52.23, lng: 21.02, id: 1 },
-      { lat: 52.21, lng: 21.0, id: 2 },
-      { lat: 52.2, lng: 21.02, id: 3 },
-      { lat: 52.24, lng: 21.03, id: 4 }
-    ]
-  }
+      { name: "restaurant1", lat: 52.23, lng: 21.02, id: 1 },
+      { name: "restaurant2", lat: 52.21, lng: 21.0, id: 2 },
+      { name: "restaurant3", lat: 52.2, lng: 21.02, id: 3 },
+      { name: "restaurant4", lat: 52.24, lng: 21.03, id: 4 }
+    ],
+    showingInfoWindow: false,
+    clickedMarker: {}
+    //selectedPlace: {}
+  };
+
+  onMarkerClick = (/*props, */ marker) =>
+    this.setState({
+      //selectedPlace: props,
+      clickedMarker: marker,
+      showInfoWindow: true
+    });
+
   render() {
     const style = {};
 
@@ -24,19 +34,24 @@ export class MapContainer extends Component {
         }}
         zoom={12}
         gestureHandling={"greedy"}
-        onClick={this.onMapClicked}
       >
         {this.state.points.map(marker => (
           <Marker
             onClick={this.onMarkerClick}
             key={marker.id}
             position={{ lat: marker.lat, lng: marker.lng }}
+            name={marker.name}
           />
         ))}
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
+        <InfoWindow
+          onClose={this.onInfoWindowClose}
+          marker={this.state.clickedMarker}
+          visible={this.state.showInfoWindow}
+        >
           <div>
-            <h1>x</h1>
+            <h1>{this.state.clickedMarker.name}</h1>
+            <img src="" alt="rest-pic" />
           </div>
         </InfoWindow>
       </Map>
